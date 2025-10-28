5bf7a664dbff5faa7cc1344a2ac2c997e65ca2b4
Carlos <cpovedasanjose@gmail.com>
2025-10-28 10:36:12 +0100

    Panel principal

--- src/main/java/org/example/tunesfx/Principal.java
+++ src/main/java/org/example/tunesfx/Principal.java
@@ -4,10 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
